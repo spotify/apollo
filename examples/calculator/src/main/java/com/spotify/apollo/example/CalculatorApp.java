3a6767e02a1b0ac0f8b34952fc5fafce746bcbdc
Rouzbeh Delavari <rouzwawi@gmail.com>
2015-11-17 22:52:55 +0100

    cleaner middleware usage

--- examples/calculator/src/main/java/com/spotify/apollo/example/CalculatorApp.java
+++ examples/calculator/src/main/java/com/spotify/apollo/example/CalculatorApp.java
@@ -1,11 +1,12 @@
 package com.spotify.apollo.example;
 
 import com.spotify.apollo.Environment;
-import com.spotify.apollo.RequestContext;
+import com.spotify.apollo.Request;
 import com.spotify.apollo.Response;
 import com.spotify.apollo.Status;
 import com.spotify.apollo.httpservice.HttpService;
 import com.spotify.apollo.httpservice.LoadingException;
+import com.spotify.apollo.route.AsyncHandler;
 import com.spotify.apollo.route.Middleware;
 import com.spotify.apollo.route.Route;
 import com.spotify.apollo.route.SyncHandler;
@@ -17,6 +18,8 @@ import java.util.Optional;
  *
  * It uses a synchronous route to evaluate addition and a middleware
  * that translates uncaught exceptions into error code 418.
+ *
+ * Try it by calling it with query parameters that are not parsable integers.
  */
 final class CalculatorApp {
 
@@ -25,16 +28,34 @@ final class CalculatorApp {
   }
 
   static void init(Environment environment) {
+    SyncHandler<Response<Integer>> addHandler = context -> add(context.request());
+
     environment.routingEngine()
-        .registerRoute(Route.with(
-            exceptionMiddleware().and(Middleware::syncToAsync), "GET", "/add", CalculatorApp::add))
-        .registerRoute(Route.sync("GET", "/unsafeadd", CalculatorApp::add));
+        .registerRoute(Route.with(exceptionHandler(), "GET", "/add", addHandler))
+        .registerRoute(Route.sync("GET", "/unsafeadd", addHandler));
+  }
+
+  /**
+   * A simple adder of request parameters {@code t1} and {@code t2}
+   *
+   * @param request  The request to handle the addition for
+   * @return A response of an integer representing the sum
+   */
+  static Response<Integer> add(Request request) {
+    Optional<String> t1 = request.parameter("t1");
+    Optional<String> t2 = request.parameter("t2");
+    if (t1.isPresent() && t2.isPresent()) {
+      int result = Integer.valueOf(t1.get()) + Integer.valueOf(t2.get());
+      return Response.forPayload(result);
+    } else {
+      return Response.forStatus(Status.BAD_REQUEST);
+    }
   }
 
   /**
    * A generic middleware that maps uncaught exceptions to error code 418
    */
-  static Middleware<SyncHandler<Response<Integer>>, SyncHandler<Response<Integer>>> exceptionMiddleware() {
+  static <T> Middleware<SyncHandler<Response<T>>, SyncHandler<Response<T>>> exceptionMiddleware() {
     return handler -> requestContext -> {
       try {
         return handler.invoke(requestContext);
@@ -44,14 +65,10 @@ final class CalculatorApp {
     };
   }
 
-  static Response<Integer> add(RequestContext context) {
-    Optional<String> t1 = context.request().parameter("t1");
-    Optional<String> t2 = context.request().parameter("t2");
-    if (t1.isPresent() && t2.isPresent()) {
-      int result = Integer.valueOf(t1.get()) + Integer.valueOf(t2.get());
-      return Response.forPayload(result);
-    } else {
-      return Response.forStatus(Status.BAD_REQUEST);
-    }
+  /**
+   * Async version of {@link #exceptionMiddleware()}
+   */
+  static <T> Middleware<SyncHandler<Response<T>>, AsyncHandler<Response<T>>> exceptionHandler() {
+    return CalculatorApp.<T>exceptionMiddleware().and(Middleware::syncToAsync);
   }
 }
