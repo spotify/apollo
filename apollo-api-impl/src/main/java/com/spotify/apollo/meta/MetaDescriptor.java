37d44b2876038ecf249fa39d3c9478cb5741506a
Rouzbeh Delavari <rouzwawi@gmail.com>
2015-11-17 17:13:25 +0100

    load service version from first manifest file on classpath

--- apollo-api-impl/src/main/java/com/spotify/apollo/meta/MetaDescriptor.java
+++ apollo-api-impl/src/main/java/com/spotify/apollo/meta/MetaDescriptor.java
@@ -18,7 +18,6 @@ public class MetaDescriptor {
 
   private static final Logger LOG = LoggerFactory.getLogger(MetaDescriptor.class);
 
-  private static final String APOLLO_VERSION = "X-Spotify-Apollo-Version";
   private static final String IMPL_VERSION = "Implementation-Version";
 
   private final Descriptor descriptor;
@@ -37,8 +36,6 @@ public class MetaDescriptor {
     return apolloVersion;
   }
 
-  // TODO: load real artifact and group id
-  // TODO: can apollo version be loaded more reliably?
   public static MetaDescriptor readMetaDescriptor(String serviceName, ClassLoader classLoader)
       throws IOException {
     String apolloVersion;
@@ -50,7 +47,7 @@ public class MetaDescriptor {
       apolloVersion = "0.0.0-UNKNOWN";
     }
 
-    version = loadVersion();
+    version = loadVersion(classLoader);
     version = version != null ? version : "0.0.0-UNKNOWN";
 
     return new MetaDescriptor(
@@ -64,7 +61,34 @@ public class MetaDescriptor {
     return properties.getProperty("apolloVersion");
   }
 
-  protected static String loadVersion() {
-    return MetaDescriptor.class.getPackage().getImplementationVersion();
+  /**
+   * Tries to load the first "Implementation-Version" manifest entry it can find in the given
+   * classloader.
+   *
+   * @param classLoader  The classloader to load manifests from
+   * @return a version string
+   * @throws IOException
+   */
+  private static String loadVersion(ClassLoader classLoader) throws IOException {
+    try {
+      Enumeration<URL> resources = classLoader.getResources("META-INF/MANIFEST.MF");
+
+      while (resources.hasMoreElements()) {
+        final URL url = resources.nextElement();
+        final Manifest manifest = new Manifest(url.openStream());
+        final Attributes mainAttributes = manifest.getMainAttributes();
+        final String value = mainAttributes.getValue(IMPL_VERSION);
+
+        if (value != null) {
+          return value;
+        }
+      }
+
+    } catch (IOException e) {
+      LOG.error("Failed to read manifest", e);
+      throw new IOException("Failed to find manifest", e);
+    }
+
+    return null;
   }
 }
