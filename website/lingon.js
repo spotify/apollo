66e180e960a13273eb4cc92ab2c8dc9e176a2927
David Flemström <dflemstr@spotify.com>
2015-11-17 11:13:01 +0100

    Dump initial website code

---
+++ website/lingon.js
@@ -0,0 +1,43 @@
+#!/usr/bin/env node
+
+var lingon = require('lingon');
+var gitDeploy = require('lingon-git-deploy');
+var lr = require('lingon-livereload');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var highlight = require('gulp-highlight');
+var through = require('through2');
+
+lr(lingon);
+
+lingon.global.title = 'Apollo';
+lingon.global.description = 'Apollo';
+lingon.global.url = 'https://spotify.github.io/apollo';
+
+// Necessary until this is fixed: https://github.com/johannestroeger/gulp-highlight/pull/5
+var unescapeQuot = function() {
+  return through.obj(function(file, enc, cont) {
+    file.contents = new Buffer(
+        file.contents.toString()
+            .replace(/&amp;quot;/g, '&quot;')
+            .replace(/&amp;gt;/g, '&gt;'));
+    this.push(file);
+    return cont();
+  });
+};
+
+lingon.config.directiveFileTypes.push('scss');
+lingon.preProcessors.push('scss', function(context, globals) {
+  return sass({includePaths: [__dirname]});
+});
+lingon.postProcessors.push('scss', function() {
+  return autoprefixer();
+});
+lingon.postProcessors.push('html', function() {
+  return [highlight(), unescapeQuot()];
+});
+
+gitDeploy(lingon, {
+  remote: 'git@github.com:spotify/apollo.git',
+  branch: 'gh-pages'
+});
