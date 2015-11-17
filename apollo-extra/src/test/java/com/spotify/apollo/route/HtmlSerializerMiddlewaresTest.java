8985b391ab88561838cb9da1296f76a76c436ce9
Mattias de Zalenski <zalenski@spotify.com>
2015-11-17 18:25:20 +0100

    Update license headers
    (removing old ones and using mvn license:update-file-header)

--- apollo-extra/src/test/java/com/spotify/apollo/route/HtmlSerializerMiddlewaresTest.java
+++ apollo-extra/src/test/java/com/spotify/apollo/route/HtmlSerializerMiddlewaresTest.java
@@ -1,3 +1,22 @@
+/*
+ * -\-\-
+ * Spotify Apollo Extra
+ * --
+ * Copyright (C) 2013 - 2015 Spotify AB
+ * --
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * -/-/-
+ */
 package com.spotify.apollo.route;
 
 import com.google.common.collect.ImmutableMap;
@@ -12,9 +31,6 @@ import okio.ByteString;
 
 import static org.junit.Assert.assertEquals;
 
-/**
- * Copyright (C) 2015 Spotify AB.
- */
 public class HtmlSerializerMiddlewaresTest {
 
   public static final String TEST_TEMPLATE_FTL = "test_template.ftl";
--- apollo-test/src/test/java/com/spotify/apollo/test/unit/RouteMatchersTest.java
+++ apollo-test/src/test/java/com/spotify/apollo/test/unit/RouteMatchersTest.java
@@ -1,3 +1,22 @@
+/*
+ * -\-\-
+ * Spotify Apollo Testing Helpers
+ * --
+ * Copyright (C) 2013 - 2015 Spotify AB
+ * --
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * -/-/-
+ */
 package com.spotify.apollo.test.unit;
 
 import com.spotify.apollo.route.AsyncHandler;
@@ -12,9 +31,6 @@ import org.junit.Test;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-/**
- * Copyright (C) 2015 Spotify AB.
- */
 public class RouteMatchersTest {
 
   private static final Route<AsyncHandler<String>> TEST_ROUTE = Route.sync("GET", "/path", (rc) -> "yo");
