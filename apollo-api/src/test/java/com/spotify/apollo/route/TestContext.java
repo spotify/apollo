0c41befef04cc83bb8e0800c004c70278373d481
Rouzbeh Delavari <rouzwawi@gmail.com>
2015-11-29 05:03:57 +0100

    add map and flatMap to SyncHandler

---
+++ apollo-api/src/test/java/com/spotify/apollo/route/TestContext.java
@@ -0,0 +1,46 @@
+/*
+ * -\-\-
+ * Spotify Apollo API Interfaces
+ * --
+ * Copyright (C) 2013 - 2015 Spotify AB
+ * --
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * -/-/-
+ */
+package com.spotify.apollo.route;
+
+import com.google.auto.value.AutoValue;
+
+import com.spotify.apollo.Client;
+import com.spotify.apollo.Request;
+import com.spotify.apollo.RequestContext;
+
+import java.util.Collections;
+import java.util.Map;
+
+@AutoValue
+abstract class TestContext implements RequestContext {
+
+  @Override
+  public Client requestScopedClient() {
+    throw new UnsupportedOperationException("no client available");
+  }
+
+  static RequestContext empty() {
+    return new AutoValue_TestContext(Request.forUri("/"), Collections.emptyMap());
+  }
+
+  static RequestContext forPathArgs(Map<String, String> pathArgs) {
+    return new AutoValue_TestContext(Request.forUri("/"), pathArgs);
+  }
+}
