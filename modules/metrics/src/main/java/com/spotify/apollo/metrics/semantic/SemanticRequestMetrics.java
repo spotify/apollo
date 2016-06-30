13548b564b436c06f98b41fa4a8485a41dd59c80
Petter Måhlén <petter@spotify.com>
2016-06-30 17:24:41 +0200

    move metrics registration out of SemanticRequestMetrics ctor
    Also adds per-endpoint caching of meters and IDs to avoid costly
    creation of MetricIds and incidentally fixes a bug that led to
    error ratios being calculated globally instead of per endpoint.

--- modules/metrics/src/main/java/com/spotify/apollo/metrics/semantic/SemanticRequestMetrics.java
+++ modules/metrics/src/main/java/com/spotify/apollo/metrics/semantic/SemanticRequestMetrics.java
@@ -19,36 +19,21 @@
  */
 package com.spotify.apollo.metrics.semantic;
 
-import com.google.common.base.Preconditions;
-
 import com.codahale.metrics.Histogram;
 import com.codahale.metrics.Meter;
-import com.codahale.metrics.RatioGauge;
-import com.codahale.metrics.RatioGauge.Ratio;
 import com.codahale.metrics.Timer;
 import com.spotify.apollo.Request;
 import com.spotify.apollo.Response;
 import com.spotify.apollo.StatusType;
 import com.spotify.apollo.metrics.RequestMetrics;
-import com.spotify.metrics.core.MetricId;
-import com.spotify.metrics.core.SemanticMetricRegistry;
 
 import java.util.Optional;
 import java.util.function.Consumer;
-import java.util.function.Predicate;
-import java.util.function.Supplier;
 
 import okio.ByteString;
 
 import static com.spotify.apollo.StatusType.Family.INFORMATIONAL;
 import static com.spotify.apollo.StatusType.Family.SUCCESSFUL;
-import static com.spotify.apollo.metrics.semantic.What.DROPPED_REQUEST_RATE;
-import static com.spotify.apollo.metrics.semantic.What.ENDPOINT_REQUEST_DURATION;
-import static com.spotify.apollo.metrics.semantic.What.ENDPOINT_REQUEST_RATE;
-import static com.spotify.apollo.metrics.semantic.What.ERROR_RATIO;
-import static com.spotify.apollo.metrics.semantic.What.REQUEST_FANOUT_FACTOR;
-import static com.spotify.apollo.metrics.semantic.What.REQUEST_PAYLOAD_SIZE;
-import static com.spotify.apollo.metrics.semantic.What.RESPONSE_PAYLOAD_SIZE;
 import static java.util.Objects.requireNonNull;
 
 // Optional fields are fine; they enable the use of the 'ifPresent' idiom which is more readable
@@ -56,120 +41,33 @@ import static java.util.Objects.requireNonNull;
 @SuppressWarnings("OptionalUsedAsFieldOrParameterType")
 class SemanticRequestMetrics implements RequestMetrics {
 
-  private final Meter sentReplies;
-  private final Meter sentErrors;
-
   private final Optional<Consumer<Response<ByteString>>> requestRateCounter;
   private final Optional<Histogram> fanoutHistogram;
   private final Optional<Histogram> requestSizeHistogram;
   private final Optional<Histogram> responseSizeHistogram;
   private final Optional<Timer.Context> timerContext;
   private final Optional<Meter> droppedRequests;
+  private final Meter sentReplies;
+  private final Meter sentErrors;
 
   SemanticRequestMetrics(
-      Predicate<What> enabledMetrics,
-      SemanticMetricRegistry metricRegistry,
-      MetricId id,
+      Optional<Consumer<Response<ByteString>>> requestRateCounter,
+      Optional<Histogram> fanoutHistogram,
+      Optional<Histogram> responseSizeHistogram,
+      Optional<Histogram> requestSizeHistogram,
+      Optional<Timer.Context> timerContext,
+      Optional<Meter> droppedRequests,
       Meter sentReplies,
       Meter sentErrors) {
 
-    // Already tagged with 'service' and 'endpoint'. 'application' gets added by the ffwd reporter
-    Preconditions.checkArgument(id.getTags().containsKey("service"),
-                                "metricId must be tagged with 'service'");
-    Preconditions.checkArgument(id.getTags().containsKey("endpoint"),
-                                "metricId must be tagged with 'endpoint'");
-
-    if (enabledMetrics.test(ENDPOINT_REQUEST_RATE)) {
-      requestRateCounter = Optional.of(response -> metricRegistry
-          .meter(id.tagged(
-              "what", ENDPOINT_REQUEST_RATE.tag(),
-              "unit", "request",
-              "status-code", String.valueOf(response.status().code())))
-          .mark());
-    } else {
-      requestRateCounter = Optional.empty();
-    }
-
-    if (enabledMetrics.test(REQUEST_FANOUT_FACTOR)) {
-      fanoutHistogram = Optional.of(
-          metricRegistry.histogram(
-              id.tagged(
-                  "what", REQUEST_FANOUT_FACTOR.tag(),
-                  "unit", "request/request")));
-    } else {
-      fanoutHistogram = Optional.empty();
-    }
-
-    if (enabledMetrics.test(RESPONSE_PAYLOAD_SIZE)) {
-      responseSizeHistogram = Optional.of(
-          metricRegistry.histogram(
-              id.tagged(
-                  "what", RESPONSE_PAYLOAD_SIZE.tag(),
-                  "unit", "B"
-              )));
-    } else {
-      responseSizeHistogram = Optional.empty();
-    }
-
-    if (enabledMetrics.test(REQUEST_PAYLOAD_SIZE)) {
-      requestSizeHistogram = Optional.of(
-          metricRegistry.histogram(
-              id.tagged(
-                  "what", REQUEST_PAYLOAD_SIZE.tag(),
-                  "unit", "B"
-              )));
-    } else {
-      requestSizeHistogram = Optional.empty();
-    }
-
-    if (enabledMetrics.test(ENDPOINT_REQUEST_DURATION)) {
-      timerContext = Optional.of(
-          metricRegistry
-              .timer(id.tagged("what", ENDPOINT_REQUEST_DURATION.tag()))
-              .time());
-    } else {
-      timerContext = Optional.empty();
-    }
-
-    if (enabledMetrics.test(DROPPED_REQUEST_RATE)) {
-      droppedRequests = Optional.of(
-          metricRegistry.meter(
-              id.tagged(
-                  "what", DROPPED_REQUEST_RATE.tag(),
-                  "unit", "request"
-              )));
-    } else {
-      droppedRequests = Optional.empty();
-    }
-
+    this.requestRateCounter = requireNonNull(requestRateCounter);
+    this.fanoutHistogram = requireNonNull(fanoutHistogram);
+    this.responseSizeHistogram = requireNonNull(responseSizeHistogram);
+    this.requestSizeHistogram = requireNonNull(requestSizeHistogram);
+    this.timerContext = requireNonNull(timerContext);
+    this.droppedRequests = requireNonNull(droppedRequests);
     this.sentReplies = requireNonNull(sentReplies);
     this.sentErrors = requireNonNull(sentErrors);
-
-    if (enabledMetrics.test(ERROR_RATIO)) {
-      registerRatioGauge(id, "1m", () -> Ratio.of(this.sentErrors.getOneMinuteRate(),
-                                                  this.sentReplies.getOneMinuteRate()),
-                         metricRegistry);
-      registerRatioGauge(id, "5m", () -> Ratio.of(this.sentErrors.getFiveMinuteRate(),
-                                                  this.sentReplies.getFiveMinuteRate()),
-                         metricRegistry);
-      registerRatioGauge(id, "15m", () -> Ratio.of(this.sentErrors.getFifteenMinuteRate(),
-                                                   this.sentReplies.getFifteenMinuteRate()),
-                         metricRegistry);
-    }
-  }
-
-  private void registerRatioGauge(MetricId metricId,
-                                  String stat,
-                                  Supplier<Ratio> ratioSupplier,
-                                  SemanticMetricRegistry metricRegistry) {
-    metricRegistry.register(
-        metricId.tagged("what", ERROR_RATIO.tag(), "stat", stat),
-        new RatioGauge() {
-          @Override
-          protected Ratio getRatio() {
-            return ratioSupplier.get();
-          }
-        });
   }
 
   @Override
