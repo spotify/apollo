08b762be27d51f520e3d7892f99ec814a340d468
Evan Shrubsole <eshr@spotify.com>
2017-01-02 15:48:29 +0100

    Add Request matcher for service field

--- apollo-test/src/test/java/com/spotify/apollo/test/unit/RequestMatchersTest.java
+++ apollo-test/src/test/java/com/spotify/apollo/test/unit/RequestMatchersTest.java
@@ -24,7 +24,10 @@ import com.spotify.apollo.Request;
 import org.hamcrest.Matcher;
 import org.junit.Test;
 
+import static org.hamcrest.CoreMatchers.endsWith;
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.CoreMatchers.nullValue;
 import static org.hamcrest.CoreMatchers.startsWith;
 import static org.junit.Assert.assertThat;
 
@@ -32,6 +35,15 @@ public class RequestMatchersTest {
 
   @Test
   public void shouldMatchUri() throws Exception {
+    Matcher<Request> requestMatcher = RequestMatchers.uri("http://hi");
+    assertThat(requestMatcher.matches(Request.forUri("http://hi")), is(true));
+    assertThat(requestMatcher.matches(Request.forUri("http://hithere")), is(false));
+    assertThat(requestMatcher.matches(Request.forUri("http://hi/there")), is(false));
+    assertThat(requestMatcher.matches(Request.forUri("http://hey")), is(false));
+  }
+
+  @Test
+  public void shouldMatchUriMatcher() throws Exception {
     Matcher<Request> requestMatcher = RequestMatchers.uri(startsWith("http://hi"));
     assertThat(requestMatcher.matches(Request.forUri("http://hi")), is(true));
     assertThat(requestMatcher.matches(Request.forUri("http://hithere")), is(true));
@@ -45,5 +57,25 @@ public class RequestMatchersTest {
 
     assertThat(requestMatcher.matches(Request.forUri("http://hi")), is(false));
     assertThat(requestMatcher.matches(Request.forUri("http://hi", "POST")), is(true));
+    assertThat(requestMatcher.matches(Request.forUri("http://hi", "GET")), is(false));
+  }
+
+  @Test
+  public void shouldMatchService() throws Exception {
+    final Matcher<Request> serviceMatcher = RequestMatchers.service("my-service");
+    final Request request = Request.forUri("http://irrelevant");
+
+    assertThat("No service", request, not(serviceMatcher));
+    assertThat("Service matches", request.withService("my-service"), serviceMatcher);
+    assertThat("Service mismatches", request.withService("not-my-service"), not(serviceMatcher));
+  }
+
+  @Test
+  public void shouldMatchServiceMatcher() throws Exception {
+    final Request request = Request.forUri("http://irrelevant");
+
+    assertThat(request, RequestMatchers.service(nullValue(String.class)));
+    assertThat(request.withService("my-service"), RequestMatchers.service(startsWith("my")));
+    assertThat(request.withService("not-my-service"), not(RequestMatchers.service(endsWith("."))));
   }
 }
