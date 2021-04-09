d807e74248e06ae2c027d0333fb4eb857f16ae40
Alina Ditu <alinad@spotify.com>
2021-04-09 14:18:17 +0200

    add readme description

--- modules/okhttp-client/src/main/java/com/spotify/apollo/http/client/HttpMetricModule.java
+++ modules/okhttp-client/src/main/java/com/spotify/apollo/http/client/HttpMetricModule.java
@@ -2,7 +2,7 @@
  * -\-\-
  * Spotify Apollo okhttp Client Module
  * --
- * Copyright (C) 2013 - 2021 Spotify AB
+ * Copyright (C) 2021 Spotify AB
  * --
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
