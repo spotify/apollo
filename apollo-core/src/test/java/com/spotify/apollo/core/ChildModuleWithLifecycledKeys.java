88384cfcde3ace0baa471ed59d03f2b10525b944
Igor Maravic <igor@spotify.com>
2021-05-24 16:59:07 +0200

    Add test

--- apollo-core/src/test/java/com/spotify/apollo/core/ChildModuleWithLifecycledKeys.java
+++ apollo-core/src/test/java/com/spotify/apollo/core/ChildModuleWithLifecycledKeys.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -27,41 +27,28 @@ import java.io.Closeable;
 import java.io.IOException;
 import java.util.concurrent.atomic.AtomicBoolean;
 
-class ModuleWithLifecycledKeys extends AbstractApolloModule {
+class ChildModuleWithLifecycledKeys extends AbstractApolloModule {
 
   private final AtomicBoolean created;
   private final AtomicBoolean closed;
-  private final AtomicBoolean childCreated;
-  private final AtomicBoolean childClosed;
 
-  public ModuleWithLifecycledKeys(
-      AtomicBoolean created,
-      AtomicBoolean closed,
-      AtomicBoolean childCreated,
-      AtomicBoolean childClosed) {
+  public ChildModuleWithLifecycledKeys(AtomicBoolean created, AtomicBoolean closed) {
     this.created = created;
     this.closed = closed;
-    this.childClosed = childClosed;
-    this.childCreated = childCreated;
-  }
-
-  public ModuleWithLifecycledKeys(AtomicBoolean created, AtomicBoolean closed) {
-    this(created, closed, new AtomicBoolean(false), new AtomicBoolean(false));
   }
 
   @Override
   protected void configure() {
-    bind(AtomicBoolean.class).annotatedWith(Names.named("created")).toInstance(created);
-    bind(AtomicBoolean.class).annotatedWith(Names.named("closed")).toInstance(closed);
+    bind(AtomicBoolean.class).annotatedWith(Names.named("childCreated")).toInstance(created);
+    bind(AtomicBoolean.class).annotatedWith(Names.named("childClosed")).toInstance(closed);
 
-    install(new ChildModuleWithLifecycledKeys(childCreated, childClosed));
     bind(Foo.class).to(FooImpl.class);
     manageLifecycle(Foo.class);
   }
 
   @Override
   public String getId() {
-    return "lifecycle-module";
+    return "child-lifecycle-module";
   }
 
   interface Foo {}
@@ -71,7 +58,8 @@ class ModuleWithLifecycledKeys extends AbstractApolloModule {
     final AtomicBoolean closed;
 
     @Inject
-    FooImpl(@Named("created") AtomicBoolean created, @Named("closed") AtomicBoolean closed) {
+    FooImpl(
+        @Named("childCreated") AtomicBoolean created, @Named("childClosed") AtomicBoolean closed) {
       this.closed = closed;
       created.set(true);
     }
