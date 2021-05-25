9b2fcd702a0cf9717ce967f04f2e33f498b49545
Igor Maravic <igor@spotify.com>
2021-05-25 12:28:28 +0200

    Fix file header

--- apollo-core/src/test/java/com/spotify/apollo/core/ChildModuleWithLifecycledKeys.java
+++ apollo-core/src/test/java/com/spotify/apollo/core/ChildModuleWithLifecycledKeys.java
@@ -2,14 +2,14 @@
  * -\-\-
  * Spotify Apollo Service Core (aka Leto)
  * --
- * Copyright (C) 2013 - 2015 Spotify AB
+ * Copyright (C) 2013 - 2020 Spotify AB
  * --
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * 
  *      http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
